37b5dd3a2be96add44146be8d96ef28882c0ad01
Tamanna-salma <tamannaummey26@gmail.com>
2025-08-24 11:48:33 +0600

    login page added

---
+++ js/login.js
@@ -0,0 +1,22 @@
+document.getElementById('login-button').addEventListener('click',function(event){
+    event.preventDefault()
+
+    const mobilenumber=12345678910
+    const pinNumber = 5678
+    // input number
+    const getNumbervalue = document.getElementById('Mobile-Number').value
+    const numberConverted = parseInt(getNumbervalue);
+
+    const getpinvalue= document.getElementById('digit-pin').value
+    const pinconverted = parseInt(getpinvalue)
+
+if(numberConverted===mobilenumber && pinconverted===pinNumber){
+    console.log('all values matched');
+    window.location.href="./home.html"
+
+}else{
+   alert('Invali credentis')
+}
+
+
+})
